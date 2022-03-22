e3e34b16746d50ce67960e0d49abfaf5ef1f4469
yournamem <email@example.com>
2022-03-22 16:46:39 +0100

    frontend kész

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# project
+# plussallatok
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,111 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table>
+      <thead>
+        <th>ID</th>
+        <th>Név</th>
+        <th>Operációk</th>
+      </thead>
+      <tbody>
+        <tr v-for="plussallat in plussallatok" :key="plussallat.id">
+          <td>{{plussallat.id}}</td>
+          <td>{{plussallat.nev}}</td>
+          <td>
+            <button @click="editPlussallat(plussallat.id)" :disabled="saving">Szerkesztés</button>
+            <button @click="deletePlussallat(plussallat.id)" :disabled="saving">Törlés</button>
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="text" v-model="plussallat.nev"></td>
+          <td>
+            <button @click="createPlussallat" :disabled="saving" v-if="!edit">Hozzáadás</button>
+            <button @click="savePlussallat" :disabled="saving" v-if="edit">Mentés</button>
+            <button @click="resetForm" :disabled="saving" v-if="edit">Mégse</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      plussallatok: [],
+      plussallat: {
+        id: null,
+        nev: ""
+      },
+      edit: false,
+      saving: false
+    }
+  },
+  methods: {
+    async listPlussallatok(){
+      let response = await fetch('http://127.0.0.1:8000/api/plussallatok')
+      let data = await response.json()
+      this.plussallatok = data
+    },
+    async createPlussallat(){
+      this.saving = true
+      await fetch('http://127.0.0.1:8000/api/plussallatok', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(this.plussallat) 
+      })
+      await this.listPlussallatok()
+      this.saving = false
+      this.resetForm()
+    },
+    async deletePlussallat(id){
+      await fetch(`http://127.0.0.1:8000/api/plussallatok/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+      await this.listPlussallatok()
+    },
+    resetForm(){
+      this.plussallat = {
+        id: null,
+        nev: ""
+      }
+      this.edit = false
+    },
+    async editPlussallat(id){
+      this.edit = true
+      let response = await fetch(`http://127.0.0.1:8000/api/plussallatok/${id}`)
+      let data = await response.json()
+      this.plussallat = {...data}
+    },
+    
+    async savePlussallat(){
+      this.saving = true
+      await fetch(`http://127.0.0.1:8000/api/plussallatok/${this.plussallat.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type' : 'application/json',
+          'Accept' : 'application/json'
+        },
+        body: JSON.stringify(this.plussallat)
+      })
+      await this.listPlussallatok()
+      this.saving = false
+      this.resetForm()
+    }
+  },
+  mounted(){
+    this.listPlussallatok()
   }
 }
 </script>
